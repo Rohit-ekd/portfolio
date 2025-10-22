3bc8ed5d7868ecec72da1a27793c62591d49a319
Rohit Varshney <136239562+Rohit-ekd@users.noreply.github.com>
2025-10-22 12:13:05 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+const printBtn = document.querySelector("#print");
+const view11 = document.querySelector("#view11");
+const view12 = document.querySelector("#view12");
+const view1 = document.querySelector("#view1");
+const view2 = document.querySelector("#view2");
+const view3 = document.querySelector("#view3");
+const view4 = document.querySelector("#view4");
+const view5 = document.querySelector("#view5");
+const view6 = document.querySelector("#view6");
+const view7 = document.querySelector("#view7");
+const view8 = document.querySelector("#view8");
+const view10 = document.querySelector("#view10");
+const view9 = document.querySelector("#view9");
+
+
+printBtn.addEventListener("click", () => {
+  window.location.href =
+    "https://drive.google.com/file/d/1OrQXi6-EAA6iJgqhB_gG5ENENTJ0t-Qp/view?usp=sharing";
+});
+
+view11.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-var/Agantic_ai_Health_Checker";
+});
+
+view12.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/AI-Virtual-Assistant";
+});
+
+view1.addEventListener("click", () => {
+  window.location.href =
+    "https://github.com/Rohit-ekd/college-management-system";
+});
+
+view2.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/coursetpoint";
+});
+
+view3.addEventListener("click", () => {
+  window.location.href =
+    "https://github.com/Rohit-var/Machinery-Maintenance-Prediction";
+});
+
+view4.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/Email-Validator";
+});
+
+view5.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/SimonGame";
+});
+
+view6.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/TinDog";
+});
+
+view7.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/RashiBeautyParlor";
+});
+// Redirect to the GitHub repository for the Auth Form project
+// when the view8 button is clicked
+view8.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/auth_form";
+});
+view10.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/studymate-bot";
+});
+
+view9.addEventListener("click", () => {
+  window.location.href = "https://github.com/Rohit-ekd/Temperature-Convetor";
+});
+
+
+
+
+
+
+// Toggle Show All Projects
